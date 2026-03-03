38f7e73b9497e759a1d034d14ee6c1507ecc1984
sajawal <sajawal75900@gmail.com>
2026-03-04 01:37:56 +0500

    update all screen added

--- porfolio-app/src/app/app.routes.ts
+++ porfolio-app/src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { SkillsComponent } from './skills/skills.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ContactsComponent } from './contacts/contacts.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'skills', component: SkillsComponent },
+  { path: 'projects', component: ProjectsComponent },
+  { path: 'contact', component: ContactsComponent },
+  { path: '**', redirectTo: '' },
+];
